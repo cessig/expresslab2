190754e691db215ae456e414dbc20826af81fb21
cessig <cessig4161994@gmail.com>
2020-09-10 16:18:22 -0400

    index route

---
+++ models/robots.js
@@ -0,0 +1,27 @@
+const robots = [
+  {
+    name: "Death Bot",
+    purpose: "Death",
+    weapons: "Flamethrower",
+    fuel: "gasoline",
+    personality: "angry",
+  },
+
+  {
+    name: "Bender",
+    purpose: "Bending",
+    weapons: "None",
+    fuel: "Alcohol",
+    personality: "Crochety",
+  },
+
+  {
+    name: "Fishing Bot",
+    purpose: "Fishing",
+    weapons: "Fishing Rod",
+    fuel: "worms",
+    personality: "Laid Back",
+  },
+];
+
+module.exports = robots;
--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+const robots = require("./models/robots");
+
+//INDEX
+app.get("/robots", (req, res) => {
+  res.render("index.ejs", { robots: robots });
+});
+
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
