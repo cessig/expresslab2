ac867d74d9ccc2cf8e9380af5fe3e30603b51934
cessig <cessig4161994@gmail.com>
2020-09-10 16:44:21 -0400

    Have show, index, and create

--- server.js
+++ server.js
@@ -4,9 +4,20 @@ const PORT = 3000;
 
 const robots = require("./models/robots");
 
+//NEW
+app.get("/robots/new", (req, res) => {
+  res.render("new.ejs");
+});
+
 //INDEX
 app.get("/robots", (req, res) => {
   res.render("index.ejs", { robots: robots });
+});
+
+//SHOW
+
+app.get("/robots/:botIndex", (req, res) => {
+  res.render("show.ejs", { oneBot: robots[req.params.botIndex] });
 });
 
 app.listen(PORT, () => {
